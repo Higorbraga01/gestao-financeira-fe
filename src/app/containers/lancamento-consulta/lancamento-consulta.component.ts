9e1882345efeb840f2df980cf529d2f754affeb9
Higor braga <higorbraga00@gmail.com>
2022-05-16 16:11:15 -0300

    Cria container de cad. e cons. de lancamento

---
+++ src/app/containers/lancamento-consulta/lancamento-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-consulta',
+  templateUrl: './lancamento-consulta.component.html',
+  styleUrls: ['./lancamento-consulta.component.scss']
+})
+export class LancamentoConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
